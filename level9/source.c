4bf13512653fb666ebd085c8551ee465c2756428
Félix JULLIEN <fjullien@student.42lyon.fr>
2026-01-19 12:17:13 +0100

    lvl9 update source

--- level9/source.c
+++ level9/source.c
@@ -17,3 +17,21 @@ void main(int param_1,int param_2)
   (*(code *)**(undefined4 **)this_00)(this_00,this);
   return;
 }
+
+void __thiscall N::N(N *this,int param_1)
+
+{
+  *(undefined ***)this = &PTR_operator+_08048848;
+  *(int *)(this + 0x68) = param_1;
+  return;
+}
+
+void __thiscall N::setAnnotation(N *this,char *param_1)
+
+{
+  size_t __n;
+  
+  __n = strlen(param_1);
+  memcpy(this + 4,param_1,__n);
+  return;
+}
